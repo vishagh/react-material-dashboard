6ce453dd51fafbd98cb5cc2a39bfe13bb1183e80
Vishagh Valavoor Girijan <Vishagh.Girijan@ad.infosys.com>
2020-11-15 21:34:14 +0530

    first dev commit with API impl and component communication with RxJS

--- src/App.js
+++ src/App.js
@@ -1,18 +1,44 @@
 import 'react-perfect-scrollbar/dist/css/styles.css';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useRoutes } from 'react-router-dom';
 import { ThemeProvider } from '@material-ui/core';
+import Snackbar from '@material-ui/core/Snackbar';
+
 import GlobalStyles from 'src/components/GlobalStyles';
 import 'src/mixins/chartjs';
 import theme from 'src/theme';
 import routes from 'src/routes';
+import {messageService} from 'src/utils/message.service';
+
 
 const App = () => {
   const routing = useRoutes(routes);
+  const [message, setMessage] = useState(null);
+  const [open, setOpen] = useState(false);
+
+  const handleClose = (event, reason) => {
+    setOpen(false);
+  };
+
+  useEffect(() => {
+    const subscription = messageService.getMessage().subscribe(text => {
+      if (text) {
+        // add message to local state if not empty
+        setMessage(text);
+        setOpen(true)
+      } 
+      else {
+        // clear messages when empty message received
+        setMessage();
+      }
+    });
+    return subscription.unsubscribe;
+  }, []);
 
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyles />
+      { message && <Snackbar onClose={handleClose} open={open} autoHideDuration={6000} message={message.text}></Snackbar> }
       {routing}
     </ThemeProvider>
   );
